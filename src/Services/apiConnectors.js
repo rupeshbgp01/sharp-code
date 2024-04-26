a61b00b7a1ffe48f6a7bbccab932550ae8c9b98a
anujdubey88 <anujd1595@gmail.com>
2024-04-26 13:50:54 +0530

    apiconnector is modified

--- src/Services/apiConnectors.js
+++ src/Services/apiConnectors.js
@@ -1,21 +1,44 @@
+// import axios from "axios";
+// import toast from "react-hot-toast";
+// axios.defaults.withCredentials = true;
+
+// export const apiConnector = async (method, url, data = null) => {
+//   try {
+//     console.log("hii from apiconnector",data);
+//     const response = await axios({
+//       method: method,
+//       url: url,
+//       data: data,
+//     },{
+//       withCredentials: true,
+//     });
+//     console.log("hii from apiconnector 2 :" ,response.data);
+//     return response;
+//   } catch (error) {
+//     throw(error);
+//     // toast.error(error.message);
+//   }
+// };
+
 import axios from "axios";
 import toast from "react-hot-toast";
 axios.defaults.withCredentials = true;
 
-export const apiConnector = async (method, url, data = null) => {
+export const apiConnector = async (method, endpoint, data = null) => {
   try {
-    console.log("hii from apiconnector",data);
+    console.log("hii from apiconnector", data);
     const response = await axios({
       method: method,
-      url: url,
+      url: `/client${endpoint}`, // Include the '/client' prefix in the URL
       data: data,
-    },{
+    }, {
       withCredentials: true,
     });
-    console.log("hii from apiconnector 2 :" ,response.data);
+    console.log("hii from apiconnector 2 :", response.data);
     return response;
   } catch (error) {
-    throw(error);
+    throw (error);
     // toast.error(error.message);
   }
 };
+
