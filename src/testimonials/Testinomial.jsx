8f34e3a9fd49022a79536c3e40bb1de314ddd32c
anujdubey88 <anujd1595@gmail.com>
2024-05-07 18:44:33 +0530

    fontstyle is changed to Inter from lato

--- src/testimonials/Testinomial.jsx
+++ src/testimonials/Testinomial.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import TestinomialCard from './TestinomialCard'
 import "./Testinomial.css"
 
-
 export default function Testinomial() {
   return (
     <div>
       <h1 className="ourcourses">TESTINOMIALS</h1>
       <div className='testinomial__main__container'>
-        <div className="scroll_infinite">
-
+        <div className="scroll_infinite marquee-content">
           <div className="card1">
             <TestinomialCard
               name="Anuj Dubey"
@@ -18,37 +16,33 @@ export default function Testinomial() {
               linkedin="https://www.linkedin.com/in/rupeshkumar2001/"
             />
           </div>
-
           <div className="card1">
-          <TestinomialCard
+            <TestinomialCard
               name="Aditya Sharma"
               degination="Manager"
               detail="Senior Manager is a high-level executive who oversees the operations and performance of one or more departments within a company. Senior Managers are responsible for developing and implementing strategies that align with the companys overall goals and objectives. They are expected to lead and motivate their team to achieve high levels of productivity and efficiency"
               linkedin="https://www.linkedin.com/in/rupeshkumar2001/"
             />
           </div>
-
           <div className="card1">
-          <TestinomialCard
+            <TestinomialCard
               name="Keshav Arora"
               degination="Senior Manager"
               detail="Senior Manager is a high-level executive who oversees the operations and performance of one or more departments within a company. Senior Managers are responsible for developing and implementing strategies that align with the companys overall goals and objectives. They are expected to lead and motivate their team to achieve high levels of productivity and efficiency"
               linkedin="https://www.linkedin.com/in/rupeshkumar2001/"
             />
           </div>
-
           <div className="card1">
-          <TestinomialCard
+            <TestinomialCard
               name="Anuj Dubey"
               degination="Senior Manager"
               detail="Senior Manager is a high-level executive who oversees the operations and performance of one or more departments within a company. Senior Managers are responsible for developing and implementing strategies that align with the companys overall goals and objectives. They are expected to lead and motivate their team to achieve high levels of productivity and efficiency"
               linkedin="https://www.linkedin.com/in/rupeshkumar2001/"
             />
           </div>
-
+          {/* Add more cards as needed */}
         </div>
       </div>
     </div>
-
   )
 }
