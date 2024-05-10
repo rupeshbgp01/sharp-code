5a8d07ee6f869ef59beadfabff1d9d01c387fcc6
anujdubey88 <anujd1595@gmail.com>
2024-05-11 00:12:37 +0530

    server link is changed

--- src/Services/apis.js
+++ src/Services/apis.js
@@ -1,13 +1,13 @@
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SIGNUP_API:   "https://server-qcrx.onrender.com/api/v2/auth/signup",
-  LOGIN_API:   "https://server-qcrx.onrender.com/api/v2/auth/login",
-  PROFILE_API: "https://server-qcrx.onrender.com/api/v2/userdetail/userDetail",
-  SUBMISSION_API: "https://server-qcrx.onrender.com/api/v2/submission",
-  UPDATE_API: "https://server-qcrx.onrender.com/api/v2/profile/updateProfile",
-  LOGOUT_API: "https://server-qcrx.onrender.com/api/v2/auth/logout",
-  EMAIL_API: "https://server-qcrx.onrender.com/api/v2/email/sendEmail",
+  SIGNUP_API:   "https://server-gksl.onrender.com/api/v2/auth/signup",
+  LOGIN_API:   "https://server-gksl.onrender.com/api/v2/auth/login",
+  PROFILE_API: "https://server-gksl.onrender.com/api/v2/userdetail/userDetail",
+  SUBMISSION_API: "https://server-gksl.onrender.com/api/v2/submission",
+  UPDATE_API: "https://server-gksl.onrender.com/api/v2/profile/updateProfile",
+  LOGOUT_API: "https://server-gksl.onrender.com/api/v2/auth/logout",
+  EMAIL_API: "https://server-gksl.onrender.com/api/v2/email/sendEmail",
   }
 
 // export const endpoints = {
